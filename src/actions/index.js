8ad4ba3c8796bf38b313bdab73ec6d5cc70db727
Mike Sausa <mikejsausa@gmail.com>
2016-10-31 15:24:54 -0600

    added authentication actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import firebase, { provider } from './firebase';
+
 export const addItem = (id, picture, question, answer, device) => {
   return {
     type: "ADD_ITEM",
@@ -14,4 +16,54 @@ export const addText = (text) => {
     type: "ADD_TEXT",
     text
   };
+};
+
+export const startListeningToAuth = (dispatch, getState) => {
+    firebase.auth().onAuthStateChanged(authData => {
+      if (authData) {
+        dispatch({
+          type: 'LOGIN',
+          uid: authData.uid,
+          username: authData.displayName
+        });
+      } else {
+        if (getState().auth.status !== 'ANONYMOUS') {
+          dispatch({
+            type: 'LOGOUT'
+          });
+        }
+      }
+    });
+  };
+
+export const logIn = (dispatch) => {
+    dispatch({
+      type: 'ATTEMPTING_LOGIN'
+    });
+
+    firebase.auth().signInWithPopup(provider)
+    .then(result => {
+      dispatch({
+        type: 'LOGIN',
+        uid: result.user.uid,
+        username: result.user.displayName
+      });
+    })
+
+    .catch(error => {
+      console.log('Error logging in: ', error);
+    });
+};
+
+export const logOut = (dispatch) => {
+    dispatch({
+      type: 'LOGOUT'
+    });
+    firebase.auth().signOut()
+    .then(() => {
+      console.log('Sign out successful!');
+    })
+    .catch(error => {
+      console.log('SIGN OUT ERROR: ', error);
+    });
 };
