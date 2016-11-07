b34e6ace39911dfdc848ff6c7c9b1faa397e545d
Mike Sausa <mikejsausa@gmail.com>
2016-11-07 15:18:26 -0700

    added login page component

---
+++ src/components/LoginPage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actions from '../actions/items';
+import SignIn from './SignIn';
+
+export class ItemsList extends React.Component {
+
+  render() {
+    const { items, deleteItem } = this.props;
+    console.log(items);
+    return (
+      <div id="sign-in-page">
+        <div id="logo"></div>
+        <h1 id="title">Memory Palace</h1>
+        <SignIn />
+      </div>
+    );
+  }
+};
+
+const mapStateToProps = (state) => state;
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(actions, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ItemsList);
